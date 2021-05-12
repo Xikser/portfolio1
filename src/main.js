9273b4f859977e3c550b70caed722770833d779c
Xikser <Xikser@wp.pl>
2021-05-12 16:14:38 +0200

    Add footer import

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import Main from "@/components/Views/Main/Main";
 import About from "@/components/Views/About/About";
 import Button from "@/components/GlobalElements/Buttons/Button";
 import Navbar from "@/components/GlobalElements/Navigation/Navbar";
+import Footer from '@/components/GlobalElements/Footer/Footer'
 
 createApp(App)
     .component('Main', Main)
     .component('About', About)
     .component('Navbar', Navbar)
     .component('Button', Button)
+    .component('Footer', Footer)
 	.use(router)
 	.mount('body')
 
