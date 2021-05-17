e1df570c8c0063029d23a1e099231a9d152fad3b
Xikser <Xikser@wp.pl>
2021-05-17 12:36:39 +0200

    Change nav mechanic

--- src/components/GlobalElements/Navigation/Navbar.vue
+++ src/components/GlobalElements/Navigation/Navbar.vue
@@ -16,18 +16,22 @@
 			</i>
 		</div>
 
+		<div class="desktop__icons">
+			<div class="bar__icon" @click="transformNavbar"></div>
+			<div class="times__icon" @click="transformNavbar"></div>
+		</div>
+
 		<ul class="navbar__list">
 			<li
 					class="navbar__item"
 					v-for="link in navItems"
 					:key="link.id"
-					@click.capture=""
 			>
 				<router-link
 						class="navbar__link"
 						:text="link.name"
 						:after-text="link.name"
-						:to="link.path"
+						:to="link.hash"
 						@click.capture="hideNavbar"
 				>
 					{{ link.name }}
@@ -38,7 +42,6 @@
 </template>
 
 <script>
-
 import {prepareRouterLinks} from '@/mixins/mixins'
 
 export default {
@@ -46,30 +49,56 @@ export default {
 	mixins: [prepareRouterLinks],
 	data() {
 		return {
-			isVisible: false
+			isVisible: false,
+			transformed: false,
 		}
 	},
-	computed: {
-		navItems() {
-			return this.globalRouterLinks.slice(0, 5)
-		},
+	mounted() {
+		window.addEventListener('scroll', this.handleScroll)
+	},
+	beforeUnmount() {
+		window.removeEventListener('scroll', this.handleScroll)
 	},
 	methods: {
+		transformNavbar() {
+			const navbar = document.querySelector('.navbar')
+			this.transformed = !this.transformed
+
+			if(this.transformed)
+				navbar.classList.add('transformed')
+			else
+				navbar.classList.remove('transformed')
+		},
+
+		handleScroll() {
+			const currentHeight = window.pageYOffset
+			const navbar = document.querySelector('.navbar')
+
+			if (currentHeight > 700)
+				navbar.classList.add('scrolled')
+			else {
+				navbar.classList.remove('transformed')
+				this.transformed = false
+				navbar.classList.remove('scrolled')
+			}
+		},
+
 		hideNavbar() {
 			const navbar = document.querySelector('.navbar')
-			if (navbar.classList.contains('visible')) {
-				navbar.classList.remove('visible')
+			if (navbar.classList.contains('mobile-visible')) {
+				navbar.classList.remove('mobile-visible')
 				this.isVisible = !this.isVisible
 			}
 		},
+
 		showNavbar() {
 			const navbar = document.querySelector('.navbar')
 			this.isVisible = !this.isVisible
 
 			if (this.isVisible === true)
-				navbar.classList.add('visible')
+				navbar.classList.add('mobile-visible')
 			else
-				navbar.classList.remove('visible')
+				navbar.classList.remove('mobile-visible')
 		},
 
 		emitEvent() {
@@ -82,111 +111,5 @@ export default {
 
 <style lang="sass" scoped>
 @import '../../../assets/sass/variables'
-
-.is-active
-	&::before
-		transform: rotate(-45deg) !important
-		z-index: -1 !important
-
-	&::after
-		transform: rotate(45deg) !important
-		z-index: -1 !important
-
-.navbar
-	position: fixed
-	width: 100%
-	height: 7em
-	z-index: 1000
-	display: flex
-	background: transparent
-	justify-content: center
-	transition: all .3s ease
-
-	&__list
-		display: flex
-		width: 60%
-		justify-content: space-evenly
-		align-items: center
-
-	&__item
-		position: relative
-		font-size: 1.5rem
-		text-transform: uppercase
-		letter-spacing: 2px
-		overflow: hidden
-
-		a
-			font-family: $font--default
-			position: relative
-			display: inline-block
-			padding: 15px 25px
-			width: 100%
-			height: 100%
-
-			&::after
-				position: absolute
-				content: ''
-				top: 50%
-				left: 0
-				width: 0
-				height: 2px
-				transition: all .3s ease
-				opacity: .7
-				background: $color--dark
-
-			&:hover::after
-				width: 100%
-
-</style>
-
-<style lang="sass" scoped>
-.hiddenIcon
-	display: none
-
-.visibleIcon
-	display: inline-block !important
-
-.visible
-	height: 100vh !important
-
-	.logo
-		top: 4% !important
-
-	.navbar__list
-		position: relative
-		display: flex
-		padding: 0
-		height: 100%
-
-.mobile__nav-icons
-	display: none
-
-@media screen and (min-width: 320px) and (max-width: 980px)
-	.mobile__nav-icons
-		position: absolute
-		height: 25px
-		display: flex
-		left: 90%
-		z-index: 501
-
-		i
-			position: relative
-			top: 90%
-			font-size: 2.5rem
-			cursor: pointer
-
-		.fa-times
-			display: none
-
-	.navbar
-		position: fixed
-		width: 100%
-		top: 0
-		left: 0
-
-		&__list
-			width: 100%
-			display: none
-			flex-direction: column
-
+@import './style'
 </style>
