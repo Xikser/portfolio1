82e968cf8cbfee10e9018ff03302d247969a830a
Xikser <Xikser@wp.pl>
2021-05-17 12:35:44 +0200

    Add hashbangs

--- src/router/index.js
+++ src/router/index.js
@@ -4,34 +4,62 @@ const routes = [
 	{
 		path: '/',
 		name: 'Strona Główna',
+		hash: '#header',
 		component: () => import('../components/Views/Main/Main')
 	},
 	{
-		path: '/about',
+		path: '/',
 		name: 'O mnie',
+		hash: '#main-about',
 		component: () => import('../components/Views/About/About')
 	},
 	{
 		path: '/',
 		name: 'Oferta',
+		hash: '#services',
 		component: () => import('../components/Views/Main/Main')
 	},
 	{
-	    path: '/',
-	    name: 'Projekty',
-	    component: () => import('../components/Views/Main/Main')
-	  },
+		path: '/',
+		name: 'Projekty',
+		hash: '#projects',
+		component: () => import('../components/Views/Main/Main')
+	},
 	{
 		path: '/',
 		name: 'Kontakt',
+		hash: '#contact',
 		component: () => import('../components/Views/Main/Main')
 	},
 ]
 
+const scrollBehavior = (to, from, savedPosition) => {
+  if (to.hash) {
+    setTimeout(() => {
+      const element = document.getElementById(to.hash.replace(/#/, ''))
+      if (element && element.scrollIntoView) {
+        element.scrollIntoView({block: 'end', behavior: 'smooth'})
+      }
+    }, 500)
+
+    //NOTE: This doesn't work for Vue 3
+    //return {selector: to.hash}
+
+    //This does
+    return {el: to.hash};
+  }
+  else if (savedPosition) {
+    return savedPosition
+  }
+  return {top: 0}
+}
+
 const router = createRouter({
+	scrollBehavior,
 	history: createWebHistory(),
-	mode: 'abstract',
+	mode: 'hash',
 	routes
 })
+
 
 export default router
