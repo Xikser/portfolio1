164013b4b0a27229d1241a03d7305c81030b9ea2
Xikser <Xikser@wp.pl>
2021-05-18 11:55:41 +0200

    Refactor

--- src/components/Views/Main/Services/Services.vue
+++ src/components/Views/Main/Services/Services.vue
@@ -3,108 +3,56 @@
 
 		<div class="placeholder-1">
 			<Button
-				buttonStyle="primary"
-				buttonSize="normal"
-				buttonText="więcej"
-				:additionalElement="true"
+					buttonStyle="primary"
+					buttonSize="normal"
+					buttonText="więcej"
+					:additionalElement="true"
+					@click="scrollTo"
 			></Button>
 		</div>
 
 		<h2 class="short">Moje usługi</h2>
+
 		<div class="services__content">
 
-			<div class="services__tile">
+			<div
+					class="services__tile"
+					v-for="(item, index) in items"
+					:key="index"
+			>
 				<div class="services__tile-heading">
-					<i class="fas fa-tv"></i>
-					<h3>Development</h3>
+					<i :class="item.icon"></i>
+					<h3>{{ item.header }}</h3>
 				</div>
 
 				<div class="services__tile-content">
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tincidunt tellus, a venenatis
-						tellus. Donec a pellentesque dui. Morbi in nibh et quam sodales accumsan. Nullam sodales ac
-						ligula vitae tristique.
+						{{ item.text }}
 					</p>
 				</div>
 			</div>
 
-			<div class="services__tile">
-				<div class="services__tile-heading">
-					<i class="fas fa-paint-brush"></i>
-					<h3>Design</h3>
-				</div>
-
-				<div class="services__tile-content">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tincidunt tellus, a venenatis
-						tellus. Donec a pellentesque dui. Morbi in nibh et quam sodales accumsan. Nullam sodales ac
-						ligula vitae tristique.
-					</p>
-				</div>
-			</div>
-
-			<div class="services__tile">
-				<div class="services__tile-heading">
-					<i class="fas fa-layer-group"></i>
-					<h3>Responsywność</h3>
-				</div>
-
-				<div class="services__tile-content">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tincidunt tellus, a venenatis
-						tellus. Donec a pellentesque dui. Morbi in nibh et quam sodales accumsan. Nullam sodales ac
-						ligula vitae tristique.
-					</p>
-				</div>
-			</div>
-
-			<div class="services__tile">
-				<div class="services__tile-heading">
-					<i class="fas fa-headset"></i>
-					<h3>Wsparcie 24/7</h3>
-				</div>
-
-				<div class="services__tile-content">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tincidunt tellus, a venenatis
-						tellus. Donec a pellentesque dui. Morbi in nibh et quam sodales accumsan. Nullam sodales ac
-						ligula vitae tristique.
-					</p>
-				</div>
-			</div>
-
-			<div class="services__tile">
-				<div class="services__tile-heading">
-					<i class="fas fa-cogs"></i>
-					<h3>Brak systemu CMS*</h3>
-				</div>
-
-				<div class="services__tile-content">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tincidunt tellus, a venenatis
-						tellus. Donec a pellentesque dui. Morbi in nibh et quam sodales accumsan. Nullam sodales ac
-						ligula vitae tristique.
-					</p>
-				</div>
-			</div>
-
-			<div class="services__tile">
-				<div class="services__tile-heading">
-					<i class="fas fa-server"></i>
-					<h3>Usługi hostingowe</h3>
-				</div>
-
-				<div class="services__tile-content">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tincidunt tellus, a venenatis
-						tellus. Donec a pellentesque dui. Morbi in nibh et quam sodales accumsan. Nullam sodales ac
-						ligula vitae tristique.
-					</p>
-				</div>
-			</div>
 		</div>
+
 	</section>
 </template>
+
+<script>
+import items from './ServicesItem'
+
+export default {
+	data() {
+		return {
+			items: items,
+		}
+	},
+	methods: {
+		scrollTo() {
+			window.scrollTo(0, 3450)
+		}
+	}
+}
+</script>
 
 <style lang="sass" scoped>
 @import './src/assets/sass/variables'
