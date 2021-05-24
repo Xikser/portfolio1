4b776e1c4a0a5c137a9838677422c88a12512269
Xikser <51416960+Xikser@users.noreply.github.com>
2021-05-24 12:43:29 +0200

    prepare animations function

---
+++ src/animations/helpers/animations.js
@@ -0,0 +1,9 @@
+const addAnimation = (e) => {
+	e.classList.add('elem-animate')
+}
+
+const removeAnimation = (e) => {
+	e.classList.remove('elem-animate')
+}
+
+export {addAnimation, removeAnimation}
