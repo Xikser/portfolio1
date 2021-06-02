5fc8b0f49503dfaa3f83c813c4db439f025fa887
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-02 12:20:42 +0200

    Change style for button type secondary

---
+++ src/components/Views/Main/Projects/ProjectNavbar.vue
@@ -0,0 +1,34 @@
+<template>
+	<nav class="project__nav">
+		<ul class="project__list">
+			<li class="project__list-item">
+				<Button
+						buttonStyle="secondary"
+						buttonSize="normal"
+						buttonText="Strony internetowe"
+				></Button>
+			</li>
+
+			<li class="project__list-item">
+				<Button
+						buttonStyle="secondary"
+						buttonSize="normal"
+						buttonText="Web design"
+				></Button>
+			</li>
+
+			<li class="project__list-item">
+				<Button
+						buttonStyle="secondary"
+						buttonSize="normal"
+						buttonText="Usługi hostingowe"
+				></Button>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<style lang="sass" scoped>
+@import './src/assets/sass/variables'
+@import 'style-nav.sass'
+</style>
