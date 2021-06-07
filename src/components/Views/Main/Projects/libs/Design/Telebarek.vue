a5616b8460a51c5a728af6aeee196726f723c476
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-07 14:44:24 +0200

    Add project

---
+++ src/components/Views/Main/Projects/libs/Design/Telebarek.vue
@@ -0,0 +1,17 @@
+<template>
+	<ProjectItem>
+		<template #image>
+			<img class="projects__image" src="../../../../../../assets/images/projects/telebarek.png" alt="">
+		</template>
+
+		<template #link>
+			<a class="projects__link">Podgląd projektu</a>
+		</template>
+	</ProjectItem>
+</template>
+<script>
+import ProjectItem from "../../ProjectItem";
+export default {
+	components: {ProjectItem}
+}
+</script>
