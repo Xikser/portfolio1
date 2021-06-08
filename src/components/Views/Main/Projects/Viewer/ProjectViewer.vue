10b8d62736ca242365e295dac926051c8311ce8a
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-08 18:00:04 +0200

    Create viewer

--- src/components/Views/Main/Projects/Viewer/ProjectViewer.vue
+++ src/components/Views/Main/Projects/Viewer/ProjectViewer.vue
@@ -1,14 +1,33 @@
 <template>
+	<section class="viewer">
+		<i class="fas fa-times" @click="$emit('offViewer', '')"></i>
 
+		<img class="viewer__image" :src="require(`@/assets/images/projects/design/${this.projectID}.jpg`)" alt="">
+	</section>
 </template>
 
 <script>
+import {preventScroll, allowScroll} from "@/mixins/mixins";
+
 export default {
+	mixins: [preventScroll, allowScroll],
+	props: {
+		item: Object,
+		projectID: String,
+	},
 	data () {
 		return {
-
+			project: null,
 		}
-	}
+	},
+	mounted() {
+		this.preventScroll(document.body)
+		this.project = this.item.children
+	},
+	beforeUnmount() {
+		this.allowScroll(document.body)
+	},
+	methods: {}
 }
 </script>
 
