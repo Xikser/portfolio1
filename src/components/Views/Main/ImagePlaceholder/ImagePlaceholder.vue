86ab211ba549613d63e8b9393acbb8a36bd55053
Xikser <51416960+Xikser@users.noreply.github.com>
2021-06-24 20:49:51 +0200

    Create image placeholder

---
+++ src/components/Views/Main/ImagePlaceholder/ImagePlaceholder.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="image-placeholder">
+		<div class="image-placeholder__image">
+			<div class="image-placeholder__horizontal">
+				<i class="line-horizontal" data-anim='draw-left'></i>
+				<i class="line-horizontal" data-anim='draw-right'></i>
+			</div>
+
+			<div class="image-placeholder__vertical">
+				<i class="line-vertical" data-anim='draw-top'></i>
+				<i class="line-vertical" data-anim='draw-bottom'></i>
+				<i class="line-vertical" data-anim='draw-top'></i>
+				<i class="line-vertical" data-anim='draw-bottom'></i>
+				<i class="line-vertical" data-anim='draw-top'></i>
+				<i class="line-vertical" data-anim='draw-bottom'></i>
+			</div>
+		</div>
+	</section>
+</template>
+
+<style lang="sass" scoped>
+@import './src/assets/sass/variables'
+@import 'style.sass'
+</style>
