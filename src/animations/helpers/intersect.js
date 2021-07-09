892ee05b9989d1f87606598275a7be7652dd36c4
Xikser <51416960+Xikser@users.noreply.github.com>
2021-07-09 14:36:43 +0200

    Add mobile width check

--- src/animations/helpers/intersect.js
+++ src/animations/helpers/intersect.js
@@ -1,15 +1,21 @@
 import {addAnimation} from './animations'
+import {handleWidth} from "@/mixins/mixins";
 
 const handleIntersect = (entry) => {
+	const mobile = handleWidth.methods.handleWidth()
 	const element = entry[0].target
 	const ratio = entry[0].intersectionRatio;
 	const boundingRect = entry[0].boundingClientRect;
 	const intersectionRect = entry[0].intersectionRect; // .top - always 0
 
-	if (ratio <= 1 && ratio > .5) {
-		if (!(boundingRect.top < intersectionRect.top)) {
-			addAnimation(element)
+	if (mobile === false) {
+		if (ratio <= 1 && ratio > .5) {
+			if (boundingRect.top === intersectionRect.top) {
+				addAnimation(element)
+			}
 		}
+	} else {
+		addAnimation(element)
 	}
 }
 
